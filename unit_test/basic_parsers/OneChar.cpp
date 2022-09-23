0480c86164dca9824bda195603b3de753730dcf1
k3DW <braden.ganetsky@gmail.com>
2022-09-23 13:21:40 -0500

    Create cleaner test utilities, and use them for OneChar tests

--- unit_test/basic_parsers/OneChar.cpp
+++ unit_test/basic_parsers/OneChar.cpp
@@ -1,135 +1,102 @@
 #include "parsers/OneChar.h"
 #include "utility.h"
 
-namespace tests::basic::OneChar
+namespace k3::tok3n::tests::basic::OneChar
 {
 
 	using k3::parser::OneChar;
 
-	template <char... c>
-	concept Constructible_From_All_Chars = (... && requires { typename OneChar<c>; });
 
-	template <char... c>
-	concept Constructible_From_Any_Chars = (... || requires { typename OneChar<c>; });
 
-	template <k3::static_string str>
-	concept Constructible_From_String = requires { typename OneChar<str>; };
+	using Single = OneChar<'a'>;
+	constexpr Single single;
 
-	consteval void single_char()
+	using Multi = OneChar<"abc">;
+	constexpr Multi multi;
+
+	void requirements_single()
 	{
-		using P = OneChar<'a'>;
-
-		static_assert(k3::parser::Parser<P>);
-		static_assert(k3::parser::IsOneChar<P>);
-		static_assert(is_result_of<P, std::string_view>);
-
-		constexpr P p;
-
-		constexpr auto r1 = P::parse("ab");
-		constexpr auto r2 = p.parse("ab");
-		static_assert(validate(success, r1, "a", "b"));
-		static_assert(validate(success, r2, "a", "b"));
-
-		constexpr auto r3 = P::parse("ba");
-		constexpr auto r4 = p.parse("ba");
-		static_assert(validate(failure, r3, "ba"));
-		static_assert(validate(failure, r4, "ba"));
+		assert
+			, is_parser<Single>
+			, parser_type_of<Single>.is_OneChar
+			, result_of<Single>.is<std::string_view>
+			, Single::parse == single.parse
+			, Single::lookahead == single.lookahead
+		;
+	}
+	void requirements_multi()
+	{
+		assert
+			, is_parser<Multi>
+			, parser_type_of<Multi>.is_OneChar
+			, result_of<Multi>.is<std::string_view>
+			, Multi::parse == multi.parse
+			, Multi::lookahead == multi.lookahead
+		;
 	}
 
-	consteval void multiple_chars()
+	void parse_single()
 	{
-		using P = OneChar<"abc">;
-
-		static_assert(k3::parser::Parser<P>);
-		static_assert(k3::parser::IsOneChar<P>);
-		static_assert(is_result_of<P, std::string_view>);
-
-		constexpr P p;
-
-		constexpr auto r1 = P::parse("abc");
-		constexpr auto r2 = P::parse("acb");
-		constexpr auto r3 = P::parse("bac");
-		constexpr auto r4 = P::parse("bca");
-		constexpr auto r5 = P::parse("cab");
-		constexpr auto r6 = P::parse("cba");
-		static_assert(validate(success, r1, "a", "bc"));
-		static_assert(validate(success, r2, "a", "cb"));
-		static_assert(validate(success, r3, "b", "ac"));
-		static_assert(validate(success, r4, "b", "ca"));
-		static_assert(validate(success, r5, "c", "ab"));
-		static_assert(validate(success, r6, "c", "ba"));
+		assert
+			, parse<Single>("ab").success("a", "b")
+			, parse<Single>("abc").success("a", "bc")
+			, parse<Single>(" abc").failure(" abc")
+			, parse<Single>("ba").failure("ba")
+		;
+	}
+	void parse_multi()
+	{
+		assert
+			, parse<Multi>("abc").success("a", "bc")
+			, parse<Multi>("acb").success("a", "cb")
+			, parse<Multi>("bac").success("b", "ac")
+			, parse<Multi>("bca").success("b", "ca")
+			, parse<Multi>("cab").success("c", "ab")
+			, parse<Multi>("cba").success("c", "ba")
+			, parse<Multi>("dcba").failure("dcba")
+			, parse<Multi>(" cba").failure(" cba")
+		;
 	}
 
-	consteval void constructible_from_ascii_only()
-	{
-		static_assert(Constructible_From_All_Chars<
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-			10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-			20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-			30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-			40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-			50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-			60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-			70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-			80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-			90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
-			100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
-			110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
-			120, 121, 122, 123, 124, 125, 126, 127>
-		);
 
-		static_assert(not Constructible_From_Any_Chars<
-			-1, -2, -3, -4, -5, -6, -7, -8, -9,
-			-10, -11, -12, -13, -14, -15, -16, -17, -18, -19,
-			-20, -21, -22, -23, -24, -25, -26, -27, -28, -29,
-			-30, -31, -32, -33, -34, -35, -36, -37, -38, -39,
-			-40, -41, -42, -43, -44, -45, -46, -47, -48, -49,
-			-50, -51, -52, -53, -54, -55, -56, -57, -58, -59,
-			-60, -61, -62, -63, -64, -65, -66, -67, -68, -69,
-			-70, -71, -72, -73, -74, -75, -76, -77, -78, -79,
-			-80, -81, -82, -83, -84, -85, -86, -87, -88, -89,
-			-90, -91, -92, -93, -94, -95, -96, -97, -98, -99,
-			-100, -101, -102, -103, -104, -105, -106, -107, -108, -109,
-			-110, -111, -112, -113, -114, -115, -116, -117, -118, -119,
-			-120, -121, -122, -123, -124, -125, -126, -127, -128>
-		);
+
+	struct constructible
+	{
+		template <class T>           static constexpr bool from_all = false;
+		template <std::size_t... Is> static constexpr bool from_all<std::index_sequence<Is...>> = (... && requires { typename OneChar<(char)Is>; });
+
+		template <class T>           static constexpr bool from_any = false;
+		template <std::size_t... Is> static constexpr bool from_any<std::index_sequence<Is...>> = (... || requires { typename OneChar<(char)Is>; });
+
+		template <k3::static_string str> static constexpr bool from_string = requires { typename OneChar<str>; };
+	};
+
+	void constructible_from_ascii_only()
+	{
+		using all_ascii_chars = std::make_index_sequence<128>;
+		using all_non_ascii_chars = decltype([]<std::size_t... Is>(std::index_sequence<Is...>) { return std::index_sequence<(Is - 128)...>{}; }(all_ascii_chars{}));
+
+		assert
+			, constructible::from_all<all_ascii_chars>
+			, not constructible::from_any<all_non_ascii_chars>
+		;
 	}
 
-	consteval void constructible_alphabetically_only()
+	void constructible_alphabetically_only()
 	{
-		static_assert(Constructible_From_String<"abc">);
-		static_assert(not Constructible_From_String<"acb">);
-		static_assert(not Constructible_From_String<"bac">);
-		static_assert(not Constructible_From_String<"bca">);
-		static_assert(not Constructible_From_String<"cab">);
-		static_assert(not Constructible_From_String<"cba">);
+		assert
+			, constructible::from_string<"abc">
+			, not constructible::from_string<"acb">
+			, not constructible::from_string<"bac">
+			, not constructible::from_string<"bca">
+			, not constructible::from_string<"cab">
+			, not constructible::from_string<"cba">
+		;
 	}
 
-	consteval void ensure_non_empty()
+	void not_constructible_empty()
 	{
-		static_assert(not Constructible_From_String<"">);
-	}
-
-	consteval void lookahead_single_char()
-	{
-		using P = OneChar<'a'>;
-
-		constexpr auto r1 = P::lookahead("ab");
-		static_assert(validate(success, r1, "b"));
-		constexpr auto r2 = P::lookahead("ba");
-		static_assert(validate(failure, r2, "ba"));
-	}
-
-	consteval void lookahead_multiple_chars()
-	{
-		using P = OneChar<"abc">;
-
-		constexpr auto r1 = P::lookahead("abc");
-		static_assert(validate(success, r1, "bc"));
-		constexpr auto r2 = P::lookahead("bac");
-		static_assert(validate(success, r2, "ac"));
-		constexpr auto r3 = P::lookahead(" bac");
-		static_assert(validate(failure, r3, " bac"));
+		assert, not constructible::from_string<"">;
 	}
 
 }
--- unit_test/utility.h
+++ unit_test/utility.h
@@ -3,6 +3,7 @@
 #include <type_traits>
 
 #include "parsers/Result.h"
+using k3::parser::Input;
 using k3::parser::Result;
 using k3::parser::failure;
 using k3::parser::success;
@@ -47,3 +48,95 @@ consteval bool validate(k3::parser::failure_t, const Result<T>& result, std::str
 
 template <class P, class R>
 concept is_result_of = std::same_as<typename P::result_type, R>;
+
+
+
+namespace k3::tok3n::tests
+{
+
+	constexpr struct assert_t
+	{
+		friend consteval assert_t operator,(assert_t self, bool b)
+		{
+			if (!b)
+				throw;
+			return self;
+		}
+	} assert;
+
+
+
+	template <class T>
+	static constexpr bool is_parser = k3::parser::Parser<T>;
+
+	template <class T>
+	struct parser_type_of_t
+	{
+		static constexpr bool is_OneChar    = k3::parser::IsOneChar<T>;
+		static constexpr bool is_NotChar    = k3::parser::IsNotChar<T>;
+		static constexpr bool is_Literal    = k3::parser::IsLiteral<T>;
+		static constexpr bool is_Choice     = k3::parser::IsChoice<T>;
+		static constexpr bool is_Sequence   = k3::parser::IsSequence<T>;
+		static constexpr bool is_OneOrMore  = k3::parser::IsOneOrMore<T>;
+		static constexpr bool is_ZeroOrMore = k3::parser::IsZeroOrMore<T>;
+		static constexpr bool is_ZeroOrOne  = k3::parser::IsZeroOrOne<T>;
+		static constexpr bool is_Transform  = k3::parser::IsTransform<T>;
+		static constexpr bool is_Flatten    = k3::parser::IsFlatten<T>;
+		static constexpr bool is_Ignore     = k3::parser::IsIgnore<T>;
+		static constexpr bool is_Delimit    = k3::parser::IsDelimit<T>;
+		static constexpr bool is_Custom     = k3::parser::IsCustom<T>;
+	};
+
+	template <class T>
+	static constexpr parser_type_of_t<T> parser_type_of{};
+
+	template <class P>
+	struct result_of_t
+	{
+		static_assert(requires { typename P::result_type; }, R"(Type used in "result_of" has no type "result_type")");
+
+		template <class R>
+		static constexpr bool is = std::same_as<typename P::result_type, R>;
+	};
+
+	template <class T>
+	static constexpr result_of_t<T> result_of{};
+
+
+
+	template <k3::parser::Parser P>
+	struct parse_t
+	{
+		consteval parse_t(Input input) : input(input) {}
+		Input input;
+
+		consteval bool success(const typename P::result_type& value, Input remaining) const
+		{
+			const auto parse_result = P::parse(input);
+			const bool parse_success = (parse_result) && (*parse_result == value) && (parse_result.remaining() == remaining);
+
+			const auto lookahead_result = P::lookahead(input);
+			const bool lookahead_success = (lookahead_result) && (lookahead_result.remaining() == remaining);
+
+			return parse_success && lookahead_success;
+		}
+
+		consteval bool failure(Input remaining) const
+		{
+			const auto parse_result = P::parse(input);
+			const bool parse_failure = (not parse_result) && (parse_result.remaining() == remaining);
+
+			const auto lookahead_result = P::lookahead(input);
+			const bool lookahead_failure = (not lookahead_result) && (lookahead_result.remaining() == remaining);
+
+			return parse_failure && lookahead_failure;
+		}
+	};
+
+	template <k3::parser::Parser P>
+	consteval parse_t<P> parse(Input input)
+	{
+		return input;
+	}
+
+}
