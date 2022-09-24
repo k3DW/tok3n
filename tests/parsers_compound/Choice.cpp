6f78ce50c2ab731d2650651df6b157e66686315f
k3DW <braden.ganetsky@gmail.com>
2022-09-24 16:36:53 -0500

    Add Choice tests

---
+++ tests/parsers_compound/Choice.cpp
@@ -0,0 +1,113 @@
+#include "pch.h"
+
+BEGIN_NAMESPACE_TOK3N(tests::compound::Choice)
+
+using k3::tok3n::Choice;
+
+
+
+struct constructible
+{
+	template <class... Ps>
+	static constexpr bool from = requires { typename Choice<Ps...>; };
+};
+
+void constructible_same_result_type()
+{
+	assert
+		, constructible::from<OneChar<'a'>, OneChar<"bc">, NotChar<'d'>, NotChar<"ef">, Literal<"literal">>
+		, not constructible::from<OneChar<"a">, Sequence<OneChar<"b">, OneChar<"c">>>
+		;
+}
+
+void not_constructible_empty()
+{
+	assert, not constructible::from<>;
+}
+
+
+
+using TwoWay1 = Choice<Literal<"ab">, NotChar<"cd">>;
+constexpr TwoWay1 twoway1;
+
+using TwoWay2 = Choice<NotChar<"cd">, Literal<"ab">>;
+constexpr TwoWay2 twoway2;
+
+using ThreeWay1 = Choice<Literal<"ab">, OneChar<"cd">, NotChar<'z'>>;
+constexpr ThreeWay1 threeway1;
+
+using ThreeWay2 = Choice<NotChar<'z'>, Literal<"ab">, OneChar<"cd">>;
+constexpr ThreeWay2 threeway2;
+
+void requirements_twoway()
+{
+	assert
+		, is_parser<TwoWay1>
+		, parser_type_of<TwoWay1>.is_Choice
+		, result_of<TwoWay1>.is<std::string_view>
+		, TwoWay1::parse == twoway1.parse
+		, TwoWay1::lookahead == twoway1.lookahead
+
+		, is_parser<TwoWay2>
+		, parser_type_of<TwoWay2>.is_Choice
+		, result_of<TwoWay2>.is<std::string_view>
+		, TwoWay2::parse == twoway2.parse
+		, TwoWay2::lookahead == twoway2.lookahead
+		;
+}
+void requirements_threeway()
+{
+	assert
+		, is_parser<ThreeWay1>
+		, parser_type_of<ThreeWay1>.is_Choice
+		, result_of<ThreeWay1>.is<std::string_view>
+		, ThreeWay1::parse == threeway1.parse
+		, ThreeWay1::lookahead == threeway1.lookahead
+
+		, is_parser<ThreeWay2>
+		, parser_type_of<ThreeWay2>.is_Choice
+		, result_of<ThreeWay2>.is<std::string_view>
+		, ThreeWay2::parse == threeway2.parse
+		, ThreeWay2::lookahead == threeway2.lookahead
+		;
+}
+
+void parse_twoway()
+{
+	assert
+		, parse<TwoWay1>("abc").success("ab", "c")
+		, parse<TwoWay1>("ba").success("b", "a")
+		, parse<TwoWay1>("cba").failure("cba")
+		, parse<TwoWay1>("dcba").failure("dcba")
+		, parse<TwoWay1>("edcba").success("e", "dcba")
+
+		, parse<TwoWay2>("abc").success("a", "bc")
+		, parse<TwoWay2>("ba").success("b", "a")
+		, parse<TwoWay2>("cba").failure("cba")
+		, parse<TwoWay2>("dcba").failure("dcba")
+		, parse<TwoWay2>("edcba").success("e", "dcba")
+		;
+}
+void parse_threeway()
+{
+	assert
+		, parse<ThreeWay1>("abc").success("ab", "c")
+		, parse<ThreeWay1>("ba").success("b", "a")
+		, parse<ThreeWay1>("cba").success("c", "ba")
+		, parse<ThreeWay1>("dcba").success("d", "cba")
+		, parse<ThreeWay1>("edcba").success("e", "dcba")
+
+		, parse<ThreeWay2>("abc").success("a", "bc")
+		, parse<ThreeWay2>("ba").success("b", "a")
+		, parse<ThreeWay2>("cba").success("c", "ba")
+		, parse<ThreeWay2>("dcba").success("d", "cba")
+		, parse<ThreeWay2>("edcba").success("e", "dcba")
+
+		, parse<ThreeWay1>("zyx").failure("zyx")
+		, parse<ThreeWay1>("xyz").success("x", "yz")
+		, parse<ThreeWay2>("zyx").failure("zyx")
+		, parse<ThreeWay2>("xyz").success("x", "yz")
+		;
+}
+
+END_NAMESPACE_TOK3N(tests::basic::NotChar)
