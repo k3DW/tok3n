c04508907df108b5afc5f43a5f6092616273f5a5
k3DW <braden.ganetsky@gmail.com>
2022-10-30 11:55:02 -0500

    Add some sub-parsers to common.h for better reuse

--- tests/common.h
+++ tests/common.h
@@ -100,31 +100,48 @@ namespace samples::all
 	using Seq3 = Sequence<L4, OC4, NC5>; constexpr Seq3 seq3;
 	using Seq4 = Sequence<NC5, L4, OC4>; constexpr Seq4 seq4;
 
-	using May1 = Maybe<L1>;                constexpr May1 may1;
-	using May2 = Maybe<OC1>;               constexpr May2 may2;
-	using May3 = Maybe<Choice<L1, OC1>>;   constexpr May3 may3;
-	using May4 = Maybe<Sequence<L1, OC1>>; constexpr May4 may4;
+	namespace Sub1
+	{
+		using _1 = L1;
+		using _2 = OC1;
+		using _3 = Choice<L1, OC1>;
+		using _4 = Sequence<L1, OC1>;
+	}
 
-	using Exa1 = Exactly<L1, 3>;                constexpr Exa1 exa1;
-	using Exa2 = Exactly<OC1, 5>;               constexpr Exa2 exa2;
-	using Exa3 = Exactly<Choice<L1, OC1>, 4>;   constexpr Exa3 exa3;
-	using Exa4 = Exactly<Sequence<L1, OC1>, 2>; constexpr Exa4 exa4;
+	using May1 = Maybe<Sub1::_1>; constexpr May1 may1;
+	using May2 = Maybe<Sub1::_2>; constexpr May2 may2;
+	using May3 = Maybe<Sub1::_3>; constexpr May3 may3;
+	using May4 = Maybe<Sub1::_4>; constexpr May4 may4;
 
-	using Oom1 = OneOrMore<L1>;                constexpr Oom1 oom1;
-	using Oom2 = OneOrMore<OC1>;               constexpr Oom2 oom2;
-	using Oom3 = OneOrMore<Choice<L1, OC1>>;   constexpr Oom3 oom3;
-	using Oom4 = OneOrMore<Sequence<L1, OC1>>; constexpr Oom4 oom4;
+	using Exa1 = Exactly<Sub1::_1, 3>; constexpr Exa1 exa1;
+	using Exa2 = Exactly<Sub1::_2, 5>; constexpr Exa2 exa2;
+	using Exa3 = Exactly<Sub1::_3, 4>; constexpr Exa3 exa3;
+	using Exa4 = Exactly<Sub1::_4, 2>; constexpr Exa4 exa4;
 
-	using Zom1 = ZeroOrMore<L1>;                constexpr Zom1 zom1;
-	using Zom2 = ZeroOrMore<OC1>;               constexpr Zom2 zom2;
-	using Zom3 = ZeroOrMore<Choice<L1, OC1>>;   constexpr Zom3 zom3;
-	using Zom4 = ZeroOrMore<Sequence<L1, OC1>>; constexpr Zom4 zom4;
+	using Oom1 = OneOrMore<Sub1::_1>; constexpr Oom1 oom1;
+	using Oom2 = OneOrMore<Sub1::_2>; constexpr Oom2 oom2;
+	using Oom3 = OneOrMore<Sub1::_3>; constexpr Oom3 oom3;
+	using Oom4 = OneOrMore<Sub1::_4>; constexpr Oom4 oom4;
 
-	using Ign1 = Ignore<ABC>;                                              constexpr Ign1 ign1;
-	using Ign2 = Ignore<OneOrMore<ABC>>;                                   constexpr Ign2 ign2;
-	using Ign3 = Ignore<Maybe<Choice<ABC, QQ>>>;                           constexpr Ign3 ign3;
-	using Ign4 = Ignore<Sequence<ABC, ZeroOrMore<QQ>>>;                    constexpr Ign4 ign4;
-	using Ign5 = Ignore<Sequence<OneOrMore<ABC>, Maybe<Choice<ABC, QQ>>>>; constexpr Ign5 ign5;
+	using Zom1 = ZeroOrMore<Sub1::_1>; constexpr Zom1 zom1;
+	using Zom2 = ZeroOrMore<Sub1::_2>; constexpr Zom2 zom2;
+	using Zom3 = ZeroOrMore<Sub1::_3>; constexpr Zom3 zom3;
+	using Zom4 = ZeroOrMore<Sub1::_4>; constexpr Zom4 zom4;
+
+	namespace Sub2
+	{
+		using _1 = ABC;
+		using _2 = OneOrMore<ABC>;
+		using _3 = Maybe<Choice<ABC, QQ>>;
+		using _4 = Sequence<ABC, ZeroOrMore<QQ>>;
+		using _5 = Sequence<OneOrMore<ABC>, Maybe<Choice<ABC, QQ>>>;
+	}
+
+	using Ign1 = Ignore<Sub2::_1>; constexpr Ign1 ign1;
+	using Ign2 = Ignore<Sub2::_2>; constexpr Ign2 ign2;
+	using Ign3 = Ignore<Sub2::_3>; constexpr Ign3 ign3;
+	using Ign4 = Ignore<Sub2::_4>; constexpr Ign4 ign4;
+	using Ign5 = Ignore<Sub2::_5>; constexpr Ign5 ign5;
 
 	using Del1 = Delimit<ABC, Comma>;    constexpr Del1 del1;
 	using Del2 = Delimit<ABC, SpaceDot>; constexpr Del2 del2;
@@ -135,20 +152,22 @@ namespace samples::all
 	using Del7 = Delimit<Comma, QQ>;     constexpr Del7 del7;
 	using Del8 = Delimit<SpaceDot, QQ>;  constexpr Del8 del8;
 
-	using Com1 = Complete<L1>;                            constexpr Com1 com1;
-	using Com2 = Complete<OC1>;                           constexpr Com2 com2;
-	using Com3 = Complete<Choice<L1, OC1>>;               constexpr Com3 com3;
-	using Com4 = Complete<Sequence<L1, OC1>>;             constexpr Com4 com4;
-	using Com5 = Complete<Maybe<Sequence<L1, OC1>>>;      constexpr Com5 com5;
-	using Com6 = Complete<OneOrMore<Sequence<L1, OC1>>>;  constexpr Com6 com6;
-	using Com7 = Complete<ZeroOrMore<Sequence<L1, OC1>>>; constexpr Com7 com7;
+	using Com1 = Complete<Sub1::_1>;             constexpr Com1 com1;
+	using Com2 = Complete<Sub1::_2>;             constexpr Com2 com2;
+	using Com3 = Complete<Sub1::_3>;             constexpr Com3 com3;
+	using Com4 = Complete<Sub1::_4>;             constexpr Com4 com4;
+	using Com5 = Complete<Maybe<Sub1::_4>>;      constexpr Com5 com5;
+	using Com6 = Complete<OneOrMore<Sub1::_4>>;  constexpr Com6 com6;
+	using Com7 = Complete<ZeroOrMore<Sub1::_4>>; constexpr Com7 com7;
 
 	static_assert(parser_equality_operator::validate(
 		oc1, oc2, oc3, nc1, nc2, nc3, l1, l2, l3, oc4, nc4, nc5, l4,
 		qq, abc, comma, spacedot,
 		cho1, cho2, cho3, cho4, seq1, seq2, seq3, seq4,
+		Sub1::_1{}, Sub1::_2{}, Sub1::_3{}, Sub1::_4{},
 		may1, may2, may3, may4, exa1, exa2, exa3, exa4,
 		oom1, oom2, oom3, oom4, zom1, zom2, zom3, zom4,
+		Sub2::_1{}, Sub2::_2{}, Sub2::_3{}, Sub2::_4{}, Sub2::_5{},
 		ign1, ign2, ign3, ign4, ign5,
 		del1, del2, del3, del4, del5, del6, del7, del8,
 		com1, com2, com3, com4, com5, com6, com7
