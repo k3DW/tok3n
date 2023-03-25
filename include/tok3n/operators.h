454c0c1339f11b81d1fac6d1c567d6ab437c4699
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-03-25 14:06:30 -0500

    #24: Refactor sequence operator to be an overload set

--- include/tok3n/operators.h
+++ include/tok3n/operators.h
@@ -3,5 +3,6 @@
 #include <tok3n/operators/Maybe.h>
 #include <tok3n/operators/modifier.h>
 #include <tok3n/operators/not.h>
+#include <tok3n/operators/Sequence.h>
 #include <tok3n/operators/OneOrMore.h>
 #include <tok3n/operators/ZeroOrMore.h>
--- include/tok3n/operators/Sequence.h
+++ include/tok3n/operators/Sequence.h
@@ -2,59 +2,31 @@
 #include <tok3n/parsers/basic/Literal.h>
 #include <tok3n/parsers/compound/Sequence.h>
 
-TOK3N_BEGIN_NAMESPACE()
+TOK3N_BEGIN_NAMESPACE(detail::operators)
 
-namespace detail::Sequence_operator
-{
+template <StaticString lhs, StaticString rhs>
+consteval auto sequence(Literal<lhs>, Literal<rhs>) { return Literal<lhs + rhs>{}; } // "ab" >> "cd" == "abcd"
 
-	template <StaticString lhs, StaticString rhs>
-	consteval auto Literal_and_Literal(Literal<lhs>, Literal<rhs>)
-	{
-		return Literal<lhs + rhs>{};
-	}
+template <Parser... P1s, Parser... P2s>
+consteval auto sequence(Sequence<P1s...>, Sequence<P2s...>) { return Sequence<P1s..., P2s...>{}; } // (P1 >> P2) >> (P3 >> P4) == (P1 >> P2 >> P3 >> P4)
 
-	template <Parser... Ps, Parser... Qs>
-	consteval auto Sequence_and_Sequence(Sequence<Ps...>, Sequence<Qs...>)
-	{
-		return Sequence<Ps..., Qs...>{};
-	}
+template <Parser... P1s, Parser P2>
+consteval auto sequence(Sequence<P1s...>, P2) { return Sequence<P1s..., P2>{}; } // (P1 >> P2) >> P3 == (P1 >> P2 >> P3)
 
-	template <Parser... Ps, Parser P>
-	consteval auto Sequence_and_anything(Sequence<Ps...>, P)
-	{
-		return Sequence<Ps..., P>{};
-	}
-
-	template <Parser P, Parser... Ps>
-	consteval auto anything_and_Sequence(P, Sequence<Ps...>)
-	{
-		return Sequence<P, Ps...>{};
-	}
-
-}
-
-inline namespace operators
-{
+template <Parser P1, Parser... P2s>
+consteval auto sequence(P1, Sequence<P2s...>) { return Sequence<P1, P2s...>{}; } // P1 >> (P2 >> P3) == (P1 >> P2 >> P3)
 
 template <Parser P1, Parser P2>
-constexpr auto operator>>(P1, P2)
+consteval auto sequence(P1, P2) { return Sequence<P1, P2>{}; } // default
+
+TOK3N_END_NAMESPACE(detail::operators)
+
+TOK3N_BEGIN_NAMESPACE(inline operators)
+
+template <Parser P1, Parser P2>
+consteval auto operator>>(P1, P2)
 {
-	using namespace detail::Sequence_operator;
-
-	if constexpr (IsLiteral<P1> && IsLiteral<P2>) // "ab" >> "cd" == "abcd"
-		return Literal_and_Literal(P1{}, P2{});
-
-	else if constexpr (IsSequence<P1> && IsSequence<P2>) // (P1 >> P2) >> (P3 >> P4) == (P1 >> P2 >> P3 >> P4)
-		return Sequence_and_Sequence(P1{}, P2{});
-	else if constexpr (IsSequence<P1>)                   // (P1 >> P2) >> P3 == (P1 >> P2 >> P3)
-		return Sequence_and_anything(P1{}, P2{});
-	else if constexpr (IsSequence<P2>)                   // P1 >> (P2 >> P3) == (P1 >> P2 >> P3)
-		return anything_and_Sequence(P1{}, P2{});
-
-	else
-		return Sequence<P1, P2>{};
+	return detail::operators::sequence(P1{}, P2{});
 }
 
-}
-
-TOK3N_END_NAMESPACE()
+TOK3N_END_NAMESPACE(inline operators)
