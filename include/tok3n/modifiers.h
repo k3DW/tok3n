a7235743efb43f9c5664d5a20fb5207f5b74ad4f
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-03-25 00:45:42 -0500

    #23: Move 'fn' modifier into its own file

--- include/tok3n/modifiers.h
+++ include/tok3n/modifiers.h
@@ -3,5 +3,6 @@
 #include <tok3n/modifiers/complete.h>
 #include <tok3n/modifiers/delimit.h>
 #include <tok3n/modifiers/exactly.h>
+#include <tok3n/modifiers/fn.h>
 #include <tok3n/modifiers/ignore.h>
 #include <tok3n/modifiers/join.h>
