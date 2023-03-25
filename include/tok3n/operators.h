c8cdabf6115d4df0da42f9f668a1f1e44010613f
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-03-25 13:58:25 -0500

    #24: Use consteval for the not operator, use namespace macros in the not file, and rename it to be lowercase

--- include/tok3n/operators.h
+++ include/tok3n/operators.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include <tok3n/operators/modifier.h>
+#include <tok3n/operators/not.h>
--- include/tok3n/operators/Not.h
+++ include/tok3n/operators/Not.h
@@ -2,22 +2,18 @@
 #include <tok3n/parsers/basic/OneChar.h>
 #include <tok3n/parsers/basic/NotChar.h>
 
-TOK3N_BEGIN_NAMESPACE()
-
-inline namespace operators
-{
+TOK3N_BEGIN_NAMESPACE(inline operators)
 
 template <StaticString str>
-constexpr auto operator!(OneChar<str>)
+consteval auto operator!(OneChar<str>)
 {
 	return NotChar<str>{};
 }
+
 template <StaticString str>
-constexpr auto operator!(NotChar<str>)
+consteval auto operator!(NotChar<str>)
 {
 	return OneChar<str>{};
 }
 
-}
-
-TOK3N_END_NAMESPACE()
+TOK3N_END_NAMESPACE(inline operators)
