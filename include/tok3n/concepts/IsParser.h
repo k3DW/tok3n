1d77cbece47bb53f50644873cee16d1db38a6ae0
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-03-25 20:00:57 -0500

    #28: Remove Is[Parser] generated concepts

--- include/tok3n/concepts/IsParser.h
+++ include/tok3n/concepts/IsParser.h
@@ -6,10 +6,4 @@ TOK3N_BEGIN_NAMESPACE()
 template <class P, ParserType type>
 concept IsParser = Parser<P> and (P::type == type);
 
-#define TOK3N_X_MACRO(TYPE)                           \
-	template <class P>                                \
-	concept Is##TYPE = IsParser<P, ParserType::TYPE>;
-TOK3N_DO_FOR_ALL_PARSER_TYPES(TOK3N_X_MACRO)
-#undef TOK3N_X_MACRO
-
 TOK3N_END_NAMESPACE()
