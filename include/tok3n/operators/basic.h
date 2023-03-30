b1be2c3c9593125aa25988e93c7abb36c4ba70af
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-03-29 19:25:19 -0500

    #27: Put basic parser constructible concepts into their own file

--- include/tok3n/operators/basic.h
+++ include/tok3n/operators/basic.h
@@ -7,46 +7,46 @@
 TOK3N_BEGIN_NAMESPACE(inline operators)
 
 template <StaticString str>
-requires detail::SingleChar_able<str>
+requires constructible::OneChar<str>
 constexpr auto one = OneChar<str>{};
 
 template <StaticString str>
-requires detail::SingleChar_able<str>
+requires constructible::NotChar<str>
 constexpr auto not_ = NotChar<str>{};
 
 template <StaticString str>
-requires detail::Literal_able<str>
+requires constructible::Literal<str>
 constexpr auto lit = Literal<str>{};
 
 template <StaticString str>
-requires detail::Literal_able<str>
+requires constructible::Literal<str>
 constexpr auto ign = Ignore<Literal<str>>{};
 
 
 
 template <StaticString str>
-requires detail::SingleChar_able<str>
+requires constructible::OneChar<str>
 consteval auto operator"" _one()
 {
 	return one<str>;
 }
 
 template <StaticString str>
-requires detail::SingleChar_able<str>
+requires constructible::NotChar<str>
 consteval auto operator"" _not()
 {
 	return not_<str>;
 }
 
 template <StaticString str>
-requires detail::Literal_able<str>
+requires constructible::Literal<str>
 consteval auto operator"" _lit()
 {
 	return lit<str>;
 }
 
 template <StaticString str>
-requires detail::Literal_able<str>
+requires constructible::Literal<str>
 consteval auto operator"" _ign()
 {
 	return ign<str>;
