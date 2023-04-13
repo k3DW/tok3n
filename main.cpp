05bf996c6bc2c3e75a9e0b5377224f6e5d99d68e
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-04-12 20:29:11 -0500

    #32: Create Algebraic example project

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <tok3n/tok3n.h>
 #include <iostream>
-#include "examples/algebraic.h"
 
 int main()
 {
 	using namespace k3;
 	using namespace k3::tok3n;
 
-	using namespace k3::tok3n::examples;
-
 	constexpr auto oc = OneChar<"ab">{};
 	constexpr auto test = exactly<2>(oc);
-
-	[[maybe_unused]] auto algtest = algebraic::test();
 
 	auto result_ = test.parse("cbac");
 	auto result_2 = test.parse("abac");
