c915c5ba3a7ef36cf271477598b2820f341f87cb
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-04-25 00:27:07 -0500

    #37: Add simple multi modifier tests

---
+++ tests/operators_modifier/multi.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+
+TOK3N_BEGIN_NAMESPACE(tests)
+
+inline void creation()
+{
+	assert
+		, ignore % join == modifiers::multi<modifiers::ignore, modifiers::join>{}
+		, join % ignore == modifiers::multi<modifiers::join, modifiers::ignore>{}
+		, complete % join == modifiers::multi<modifiers::complete, modifiers::join>{}
+		, join % complete == modifiers::multi<modifiers::join, modifiers::complete>{}
+		, ignore % complete == modifiers::multi<modifiers::ignore, modifiers::complete>{}
+		, complete % ignore == modifiers::multi<modifiers::complete, modifiers::ignore>{}
+		;
+}
+
+inline void not_commutative()
+{
+	assert
+		, (ignore % join) != (join % ignore)
+		, (complete % join) != (join % complete)
+		, (ignore % complete) != (complete % ignore)
+		;
+}
+
+inline void associative()
+{
+	assert
+		, ((ignore % join) % complete) % into<Sink> == (ignore % (join % complete)) % into<Sink>
+		, ((ignore % join) % complete) % into<Sink> == (ignore % join) % (complete % into<Sink>)
+		, ((ignore % join) % complete) % into<Sink> == ignore % ((join % complete) % into<Sink>)
+		, ((ignore % join) % complete) % into<Sink> == ignore % (join % (complete % into<Sink>))
+		;
+}
+
+inline void applying()
+{
+	assert
+		, oom1 % (join % ignore % complete) == Complete<Ignore<Join<Oom1>>>{}
+		, oom1 % (join % complete % ignore) == Ignore<Complete<Join<Oom1>>>{}
+		;
+}
+
+TOK3N_END_NAMESPACE(tests)
