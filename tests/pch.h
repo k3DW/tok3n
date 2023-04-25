2270e46db2078bf05214f5401ee099944a4c501b
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-04-24 22:54:02 -0500

    #40: Put newly introduces utilities into their proper files

--- tests/pch.h
+++ tests/pch.h
@@ -5,6 +5,7 @@
 #include "framework/assert.h"
 #include "framework/comparison.h"
 #include "framework/parse.h"
+#include "framework/underlying.h"
 #include "samples/adaptor.h"
 #include "samples/basic.h"
 #include "samples/classes.h"
@@ -16,30 +17,6 @@
 TOK3N_BEGIN_NAMESPACE(tests)
 
 constexpr auto the_parser_list = adaptor_list + basic_list + compound_list + divergent_list + repeat_list;
-
-#define TOK3N_ASSERT_P(condition, message) \
-	do {                                   \
-		if constexpr (not (condition))     \
-			typename P::__error_with_type; \
-	} while(false)
-
-
-struct underlying
-{
-private:
-	template <template <class> class ParserTemplate, Parser P>
-	static consteval auto impl(ParserTemplate<P>) { return P{}; }
-
-	template <template <StaticString> class ParserTemplate, StaticString str>
-	static consteval auto impl(ParserTemplate<str>) { return str; }
-
-public:
-	template <Parser P>
-	using parser = decltype(impl(P{}));
-
-	template <Parser P>
-	static constexpr auto string = impl(P{});
-};
 
 consteval bool check_all_samples(auto checker)
 {
