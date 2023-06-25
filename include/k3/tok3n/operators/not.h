d405a7b0c270a3cf55d6a06b14b16c45dd4bda02
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-06-25 18:24:36 -0500

    #69: Put not operator into k3::tok3n namespace

--- include/k3/tok3n/operators/not.h
+++ include/k3/tok3n/operators/not.h
@@ -2,7 +2,7 @@
 #include <k3/tok3n/parsers/basic/OneChar.h>
 #include <k3/tok3n/parsers/basic/NotChar.h>
 
-namespace k3::tok3n::inline operators {
+namespace k3::tok3n {
 
 template <StaticString str>
 consteval auto operator!(OneChar<str>)
@@ -19,4 +19,10 @@ consteval auto operator!(NotChar<str>)
 template <Parser P>
 consteval auto operator!(P) = delete;
 
-} // namespace k3::tok3n::inline operators
+} // namespace k3::tok3n
+
+namespace k3::tok3n::operators {
+
+using ::k3::tok3n::operator!;
+
+} // namespace k3::tok3n::operators
