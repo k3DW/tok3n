246d774c2129767af13ac01ec148ec99bbf49e10
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-06-25 18:24:17 -0500

    #69: Put maybe operator into k3::tok3n namespace

--- include/k3/tok3n/operators/maybe.h
+++ include/k3/tok3n/operators/maybe.h
@@ -3,7 +3,7 @@
 #include <k3/tok3n/parsers/repeat/OneOrMore.h>
 #include <k3/tok3n/parsers/repeat/ZeroOrMore.h>
 
-namespace k3::tok3n::detail::operators {
+namespace k3::tok3n::operators_impl {
 
 template <Parser P>
 consteval auto maybe(Maybe<P>) { return Maybe<P>{}; } // ~(~P) == ~P
@@ -17,15 +17,21 @@ consteval auto maybe(ZeroOrMore<P>) { return ZeroOrMore<P>{}; } // ~(*P) == *P
 template <Parser P>
 consteval auto maybe(P) { return Maybe<P>{}; } // default
 
-} // namespace k3::tok3n::detail::operators
+} // namespace k3::tok3n::operators_impl
 
-namespace k3::tok3n::inline operators {
+namespace k3::tok3n {
 
 template <Parser P>
 requires constructible::Maybe<P>
 consteval auto operator~(P)
 {
-	return detail::operators::maybe(P{});
+	return operators_impl::maybe(P{});
 }
 
-} // namespace k3::tok3n::inline operators
+} // namespace k3::tok3n
+
+namespace k3::tok3n::operators {
+
+using ::k3::tok3n::operator~;
+
+} // namespace k3::tok3n::operators
