ba9b0eb1a08b54f3c0c74b1cadb1119d52789b91
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-06-25 15:46:01 -0500

    #56: Put has_tuple_size into detail namespace

--- include/k3/tok3n/concepts.h
+++ include/k3/tok3n/concepts.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <k3/tok3n/concepts/has_tuple_size.h>
 #include <k3/tok3n/concepts/implicitly_default_constructible.h>
 #include <k3/tok3n/concepts/is_bool_constant.h>
 #include <k3/tok3n/concepts/IsParser.h>
