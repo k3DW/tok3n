0d20d74f7ef57a787148c16bf1ebf4d9cdac6a95
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-06-25 15:47:49 -0500

    #56: Put implicitly_default_constructible into detail namespace

--- include/k3/tok3n/concepts/Parser.h
+++ include/k3/tok3n/concepts/Parser.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <k3/tok3n/types/ParserType.h>
 #include <k3/tok3n/types/Input.h>
-#include <k3/tok3n/concepts/implicitly_default_constructible.h>
+#include <k3/tok3n/detail/implicitly_default_constructible.h>
 #include <k3/tok3n/concepts/IsResult.h>
 #include <type_traits>
 
@@ -13,7 +13,7 @@ concept Parser =
 	static_cast<int>(P::type) > static_cast<int>(ParserType::None) &&
 	static_cast<int>(P::type) < static_cast<int>(ParserType::END) &&
 	(std::is_empty_v<P>) &&
-	implicitly_default_constructible<P> &&
+	detail::implicitly_default_constructible<P> &&
 	requires { typename P::result_type; } &&
 	requires (Input input)
 	{
