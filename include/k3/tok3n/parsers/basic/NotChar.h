00a3c86903b84b457ab2b800592726fc7ff59adb
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-06-25 23:32:28 -0500

    #63: Create a forward declaration file for basic parsers, and reimplement basic parser generalization with traits

--- include/k3/tok3n/parsers/basic/NotChar.h
+++ include/k3/tok3n/parsers/basic/NotChar.h
@@ -1,22 +1,26 @@
 #pragma once
-#include <k3/tok3n/types.h>
-#include <k3/tok3n/concepts.h>
-#include <k3/tok3n/parsers/_base/Basic.h>
+#include <k3/tok3n/parsers/basic/fwd.h>
+#include <k3/tok3n/parsers/basic/BasicBase.h>
 
 namespace k3::tok3n {
 
 template <StaticString str>
-requires (is_ascii(str)) and (is_sorted_and_uniqued(str))
-struct NotChar : detail::Basic<NotChar<str>>
+requires NotCharConstructible<str>
+struct BasicTraits<NotChar<str>>
 {
-	static constexpr ParserType type = NotCharType;
+	static constexpr std::size_t length = 1;
 
-	static constexpr std::size_t _length = 1;
-
-	static constexpr bool _failure_condition(Input input)
+	static constexpr bool failure_condition(Input input)
 	{
 		return input.empty() || str.contains(input.front());
 	}
+};
+
+template <StaticString str>
+requires NotCharConstructible<str>
+struct NotChar : BasicBase<NotChar<str>>
+{
+	static constexpr ParserType type = NotCharType;
 };
 
 } // namespace k3::tok3n
