334efd4e74f5ac6a2a24df2a059325706c87f580
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-06-27 17:57:45 -0500

    #78: Remove the is_ascii function

--- include/k3/tok3n/parsers/basic/_fwd.h
+++ include/k3/tok3n/parsers/basic/_fwd.h
@@ -5,13 +5,13 @@
 namespace k3::tok3n {
 
 template <StaticString str>
-concept OneCharConstructible = (is_ascii(str)) and (is_sorted_and_uniqued(str));
+concept OneCharConstructible = is_sorted_and_uniqued(str);
 
 template <StaticString str>
-concept NotCharConstructible = (is_ascii(str)) and (is_sorted_and_uniqued(str));
+concept NotCharConstructible = is_sorted_and_uniqued(str);
 
 template <StaticString str>
-concept LiteralConstructible = (is_ascii(str));
+concept LiteralConstructible = true;
 
 
 
