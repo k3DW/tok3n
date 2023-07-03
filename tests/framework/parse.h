7ccf98a2541c1bac8fa59e89185cc2935d323453
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-07-03 13:00:12 -0500

    #77: Create assertion macros for parsing

--- tests/framework/parse.h
+++ tests/framework/parse.h
@@ -79,3 +79,72 @@ consteval auto parse(P, Input input)
 {
 	return parse_t<P>{ input };
 }
+
+#define TOK3N_ASSERT_PARSER_CONCEPT(P)                   \
+	static_assert(Parser<P>,                             \
+		"(" #P ") does not satisfy the Parser concept.")
+
+#define TOK3N_ASSERT_PARSE_VALID(P, INPUT)                                           \
+	static_assert((P::parse(INPUT)).has_value(),                                     \
+		"(" #P "::parse(" #INPUT ")) does not give a valid result, but it should." )
+
+#define TOK3N_ASSERT_PARSE_INVALID(P, INPUT)                                     \
+	static_assert(not (P::parse(INPUT)).has_value(),                             \
+		"(" #P "::parse(" #INPUT ")) gives a valid result, but it should not." )
+
+#define TOK3N_ASSERT_PARSE_RESULT(P, INPUT, OUTPUT)                  \
+	static_assert(*(P::parse(INPUT)) == (OUTPUT),                    \
+		"*(" #P "::parse(" #INPUT ")) does not equal (" #OUTPUT ")")
+
+#define TOK3N_ASSERT_PARSE_REMAINING(P, INPUT, REMAINING)                          \
+	static_assert((P::parse(INPUT)).remaining() == (REMAINING),                    \
+		"(" #P "::parse(" #INPUT ")).remaining() does not equal (" #REMAINING ")")
+
+#define TOK3N_ASSERT_LOOKAHEAD_VALID(P, INPUT)                                           \
+	static_assert((P::lookahead(INPUT)).has_value(),                                     \
+		"(" #P "::lookahead(" #INPUT ")) does not give a valid result, but it should." )
+
+#define TOK3N_ASSERT_LOOKAHEAD_INVALID(P, INPUT)                                     \
+	static_assert(not (P::lookahead(INPUT)).has_value(),                             \
+		"(" #P "::lookahead(" #INPUT ")) gives a valid result, but it should not." )
+
+#define TOK3N_ASSERT_LOOKAHEAD_REMAINING(P, INPUT, REMAINING)                          \
+	static_assert((P::lookahead(INPUT)).remaining() == (REMAINING),                    \
+		"(" #P "::lookahead(" #INPUT ")).remaining() does not equal (" #REMAINING ")")
+
+#define TOK3N_ASSERT_SUCCESS(P, INPUT, OUTPUT, REMAINING)      \
+	do {                                                       \
+		TOK3N_ASSERT_PARSER_CONCEPT(P);                        \
+		TOK3N_ASSERT_PARSE_VALID(P, INPUT);                    \
+		TOK3N_ASSERT_PARSE_RESULT(P, INPUT, OUTPUT);           \
+		TOK3N_ASSERT_PARSE_REMAINING(P, INPUT, REMAINING);     \
+		TOK3N_ASSERT_LOOKAHEAD_VALID(P, INPUT);                \
+		TOK3N_ASSERT_LOOKAHEAD_REMAINING(P, INPUT, REMAINING); \
+	} while (false)
+
+#define TOK3N_ASSERT_SUCCESS_VOID(P, INPUT, REMAINING)         \
+	do {                                                       \
+		TOK3N_ASSERT_PARSER_CONCEPT(P);                        \
+		TOK3N_ASSERT_PARSE_VALID(P, INPUT);                    \
+		TOK3N_ASSERT_PARSE_REMAINING(P, INPUT, REMAINING);     \
+		TOK3N_ASSERT_LOOKAHEAD_VALID(P, INPUT);                \
+		TOK3N_ASSERT_LOOKAHEAD_REMAINING(P, INPUT, REMAINING); \
+	} while (false)
+
+#define TOK3N_ASSERT_FAILURE(P, INPUT)                     \
+	do {                                                   \
+		TOK3N_ASSERT_PARSER_CONCEPT(P);                    \
+		TOK3N_ASSERT_PARSE_INVALID(P, INPUT);              \
+		TOK3N_ASSERT_PARSE_REMAINING(P, INPUT, INPUT);     \
+		TOK3N_ASSERT_LOOKAHEAD_INVALID(P, INPUT);          \
+		TOK3N_ASSERT_LOOKAHEAD_REMAINING(P, INPUT, INPUT); \
+	} while (false)
+
+#define TOK3N_ASSERT_LOOKAHEAD_ONLY(P, INPUT, REMAINING)       \
+	do {                                                       \
+		TOK3N_ASSERT_PARSER_CONCEPT(P);                        \
+		TOK3N_ASSERT_PARSE_INVALID(P, INPUT);                  \
+		TOK3N_ASSERT_PARSE_REMAINING(P, INPUT, INPUT);         \
+		TOK3N_ASSERT_LOOKAHEAD_VALID(P, INPUT);                \
+		TOK3N_ASSERT_LOOKAHEAD_REMAINING(P, INPUT, REMAINING); \
+	} while (false)
