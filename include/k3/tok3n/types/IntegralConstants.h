a81131b3dab55646f360712eea122a8f9040b306
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-07-15 15:27:15 -0500

    #92: Add alias Const for a std::integral_constant without needing to specify the type

--- include/k3/tok3n/types/IntegralConstants.h
+++ include/k3/tok3n/types/IntegralConstants.h
@@ -6,6 +6,9 @@ namespace k3::tok3n {
 template <std::size_t N>
 using Index = std::integral_constant<std::size_t, N>;
 
+template <auto val>
+using Const = std::integral_constant<decltype(val), val>;
+
 using True  = std::true_type;
 using False = std::false_type;
 
