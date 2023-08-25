4486931ca9c2adb8d95fa6730b0fdedb8e37983b
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-08-25 17:28:51 -0500

    #43: Add tests for Join OneOrMore and ZeroOrMore contiguous parsers

---
+++ tests/parsers_optimizations/JoinContiguous.cpp
@@ -0,0 +1,76 @@
+#include "pch.h"
+
+FIXTURE("JoinContiguous");
+
+using Joi_Oom_OC  = Join<OneOrMore<OneChar<"123">>>;
+using Joi_Oom_NC  = Join<OneOrMore<NotChar<"123">>>;
+using Joi_Oom_L   = Join<OneOrMore<Literal<"123">>>;
+using Joi_Zom_OC  = Join<ZeroOrMore<OneChar<"123">>>;
+using Joi_Zom_NC  = Join<ZeroOrMore<NotChar<"123">>>;
+using Joi_Zom_L   = Join<ZeroOrMore<Literal<"123">>>;
+
+TEST("JoinContiguous", "Requirements")
+{
+    ASSERT_IS_PARSER(Joi_Oom_OC, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Oom_NC, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Oom_L, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Zom_OC, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Zom_NC, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Zom_L, JoinType, std::string_view);
+}
+
+TEST("JoinContiguous", "OneOrMore<OneChar>")
+{
+    ASSERT_PARSE_SUCCESS(Joi_Oom_OC, "12321321", "12321321", "");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_OC, "1232 1321", "1232", " 1321");
+    ASSERT_PARSE_FAILURE(Joi_Oom_OC, " 12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_OC, "12341321", "123", "41321");
+    ASSERT_PARSE_FAILURE(Joi_Oom_OC, "012341321");
+}
+
+TEST("JoinContiguous", "OneOrMore<NotChar>")
+{
+    ASSERT_PARSE_FAILURE(Joi_Oom_NC, "12321321");
+    ASSERT_PARSE_FAILURE(Joi_Oom_NC, "1232 1321");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_NC, " 12321321", " ", "12321321");
+    ASSERT_PARSE_FAILURE(Joi_Oom_NC, "12341321");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_NC, "012341321", "0", "12341321");
+}
+
+TEST("JoinContiguous", "OneOrMore<Literal>")
+{
+    ASSERT_PARSE_SUCCESS(Joi_Oom_L, "12321321", "123", "21321");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_L, "12312321321", "123123", "21321");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_L, "1232 1321", "123", "2 1321");
+    ASSERT_PARSE_FAILURE(Joi_Oom_L, " 12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Oom_L, "12341321", "123", "41321");
+    ASSERT_PARSE_FAILURE(Joi_Oom_L, "012341321");
+}
+
+TEST("JoinContiguous", "ZeroOrMore<OneChar>")
+{
+    ASSERT_PARSE_SUCCESS(Joi_Zom_OC, "12321321", "12321321", "");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_OC, "1232 1321", "1232", " 1321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_OC, " 12321321", "", " 12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_OC, "12341321", "123", "41321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_OC, "012341321", "", "012341321");
+}
+
+TEST("JoinContiguous", "ZeroOrMore<NotChar>")
+{
+    ASSERT_PARSE_SUCCESS(Joi_Zom_NC, "12321321", "", "12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_NC, "1232 1321", "", "1232 1321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_NC, " 12321321", " ", "12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_NC, "12341321", "", "12341321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_NC, "012341321", "0", "12341321");
+}
+
+TEST("JoinContiguous", "ZeroOrMore<Literal>")
+{
+    ASSERT_PARSE_SUCCESS(Joi_Zom_L, "12321321", "123", "21321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_L, "12312321321", "123123", "21321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_L, "1232 1321", "123", "2 1321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_L, " 12321321", "", " 12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_L, "12341321", "123", "41321");
+    ASSERT_PARSE_SUCCESS(Joi_Zom_L, "012341321", "", "012341321");
+}
