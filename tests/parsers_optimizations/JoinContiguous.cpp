eabfd7da8a4cbd809277787aa2c1adb3872a891a
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-08-25 18:49:42 -0500

    #43: Add tests for Join Exactly contiguous parsers

--- tests/parsers_optimizations/JoinContiguous.cpp
+++ tests/parsers_optimizations/JoinContiguous.cpp
@@ -11,6 +11,9 @@ using Joi_Zom_L   = Join<ZeroOrMore<Literal<"123">>>;
 using Joi_May_OC  = Join<Maybe<OneChar<"123">>>;
 using Joi_May_NC  = Join<Maybe<NotChar<"123">>>;
 using Joi_May_L   = Join<Maybe<Literal<"123">>>;
+using Joi_Exa_OC  = Join<Exactly<OneChar<"123">, Index<2>>>;
+using Joi_Exa_NC  = Join<Exactly<NotChar<"123">, Index<2>>>;
+using Joi_Exa_L   = Join<Exactly<Literal<"123">, Index<2>>>;
 
 TEST("JoinContiguous", "Requirements")
 {
@@ -23,6 +26,9 @@ TEST("JoinContiguous", "Requirements")
     ASSERT_IS_PARSER(Joi_May_OC, JoinType, std::string_view);
     ASSERT_IS_PARSER(Joi_May_NC, JoinType, std::string_view);
     ASSERT_IS_PARSER(Joi_May_L, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Exa_OC, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Exa_NC, JoinType, std::string_view);
+    ASSERT_IS_PARSER(Joi_Exa_L, JoinType, std::string_view);
 }
 
 TEST("JoinContiguous", "OneOrMore<OneChar>")
@@ -107,4 +113,36 @@ TEST("JoinContiguous", "Maybe<Literal>")
     ASSERT_PARSE_SUCCESS(Joi_May_L, " 12321321", "", " 12321321");
     ASSERT_PARSE_SUCCESS(Joi_May_L, "12341321", "123", "41321");
     ASSERT_PARSE_SUCCESS(Joi_May_L, "012341321", "", "012341321");
+}
+
+TEST("JoinContiguous", "Exactly<OneChar, 2>")
+{
+    ASSERT_PARSE_SUCCESS(Joi_Exa_OC, "12321321", "12", "321321");
+    ASSERT_PARSE_SUCCESS(Joi_Exa_OC, "1232 1321", "12", "32 1321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_OC, " 12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Exa_OC, "12341321", "12", "341321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_OC, "012341321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_OC, "0012341321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_OC, "1012341321");
+}
+
+TEST("JoinContiguous", "Exactly<NotChar, 2>")
+{
+    ASSERT_PARSE_FAILURE(Joi_Exa_NC, "12321321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_NC, "1232 1321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_NC, " 12321321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_NC, "12341321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_NC, "012341321");
+    ASSERT_PARSE_SUCCESS(Joi_Exa_NC, "0012341321", "00", "12341321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_NC, "1012341321");
+}
+
+TEST("JoinContiguous", "Exactly<Literal, 2>")
+{
+    ASSERT_PARSE_FAILURE(Joi_Exa_L, "12321321");
+    ASSERT_PARSE_SUCCESS(Joi_Exa_L, "12312321321", "123123", "21321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_L, "1232 1321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_L, " 12321321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_L, "12341321");
+    ASSERT_PARSE_FAILURE(Joi_Exa_L, "012341321");
 }
