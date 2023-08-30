af1f7dc5dce238676cc51595a5f58b406da6e29e
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-08-29 21:41:34 -0500

    #102: Remove accidentally left-over macros TOK3N_ASSERT_P and TOK3N_ASSERT_P2

--- tests/framework/Assert.h
+++ tests/framework/Assert.h
@@ -22,20 +22,3 @@
 #define IDENTITY(...) __VA_ARGS__
 
 #define STR(...) #__VA_ARGS__
-
-
-
-#define TOK3N_ASSERT_P(condition, message) \
-	do {                                   \
-		if constexpr (not (condition))     \
-			typename P::__error_with_type; \
-	} while(false)
-
-#define TOK3N_ASSERT_P2(condition, message)      \
-	do {                                         \
-		if constexpr (not (condition))           \
-		{                                        \
-			typename LHS::__error_with_type_LHS; \
-			typename RHS::__error_with_type_RHS; \
-		}                                        \
-	} while(false)
