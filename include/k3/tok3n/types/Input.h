56fdd1fe5905680834e1e417a100b4e2e573f83f
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-09-04 15:29:59 -0500

    #101: Replace the Input alias with an Input class, as a simple wrapper around std::string_view

--- include/k3/tok3n/types/Input.h
+++ include/k3/tok3n/types/Input.h
@@ -3,6 +3,43 @@
 
 namespace k3::tok3n {
 
-using Input = std::string_view;
+class Input
+{
+public:
+	constexpr Input() = default;
+
+	constexpr Input(std::string_view value)
+		: _value(value)
+	{}
+
+	template <std::size_t N>
+	constexpr Input(const char(&arr)[N])
+		: _value(arr)
+	{}
+
+	constexpr Input(const char* data, std::size_t size)
+		: _value(data, size)
+	{}
+
+	constexpr operator std::string_view() const
+	{
+		return _value;
+	}
+
+	constexpr auto data() const { return _value.data(); }
+	constexpr auto size() const { return _value.size(); }
+	constexpr auto empty() const { return _value.empty(); }
+	constexpr auto& front() const { return _value.front(); }
+
+	template <class U>
+	constexpr auto starts_with(U u) const { return _value.starts_with(u); }
+
+	constexpr void remove_prefix(std::size_t count) { _value.remove_prefix(count); }
+
+	friend constexpr bool operator==(const Input&, const Input&) = default;
+
+private:
+	std::string_view _value;
+};
 
 } // namespace k3::tok3n
