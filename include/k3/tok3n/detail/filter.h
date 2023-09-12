2f7568002baa6311ee1fcae3a7c0c774be15e412
Braden Ganetsky <braden.ganetsky@gmail.com>
2023-09-11 22:13:01 -0500

    #116: Remove list_cast

--- include/k3/tok3n/detail/filter.h
+++ include/k3/tok3n/detail/filter.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <k3/tok3n/detail/invoke_type.h>
-#include <k3/tok3n/detail/list_cast.h>
 #include <k3/tok3n/detail/type_list.h>
 #include <k3/tok3n/detail/type_predicate.h>
 #include <tuple>
@@ -8,28 +7,28 @@
 namespace k3::tok3n::detail {
 
 constexpr auto filter_impl =
-	[]<class Self, type_predicate Pred, class... Done, class Head, class... Tail>
-		(Self self, Pred, type_list<Done...>, std::type_identity<Head>, std::type_identity<Tail>... tail) consteval
+	[]<class Self, template <class...> class List, type_predicate Pred, class... Done, class Head, class... Tail>
+		(Self self, Pred, List<Done...>, std::type_identity<Head>, std::type_identity<Tail>... tail) consteval
 	{
 		throw;
 
 		if constexpr (sizeof...(Tail) == 0)
 		{
 			if constexpr (Pred::template predicate<Head>::value)
-				return type_list<Done..., std::type_identity<Head>>{};
+				return List<Done..., Head>{};
 			else
-				return type_list<Done...>{};
+				return List<Done...>{};
 		}
 		else
 		{
 			if constexpr (Pred::template predicate<Head>::value)
-				return self(self, Pred{}, type_list<Done..., std::type_identity<Head>>{}, tail...);
+				return self(self, Pred{}, List<Done..., Head>{}, tail...);
 			else
-				return self(self, Pred{}, type_list<Done...>{}, tail...);
+				return self(self, Pred{}, List<Done...>{}, tail...);
 		}
 	};
 
 template <type_predicate Pred, template <class...> class List, class... Ts>
-using filter = list_cast<List, invoke_type<filter_impl, decltype(filter_impl), Pred, type_list<>, std::type_identity<Ts>...>>;
+using filter = invoke_type<filter_impl, decltype(filter_impl), Pred, List<>, std::type_identity<Ts>...>;
 
 } // namespace k3::tok3n::detail
