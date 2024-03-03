84d78ba98d7480996cd7f7e52ce42c230b11438d
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-03-03 15:47:45 -0600

    #133: Add any_of, none_of, and all_of

--- include/k3/tok3n/operators/basic.h
+++ include/k3/tok3n/operators/basic.h
@@ -9,11 +9,23 @@ namespace k3::tok3n {
 template <StaticArray arr>
 constexpr auto any = AnyOf<detail::sort_and_unique<arr>()>{};
 
+template <auto t, auto... ts>
+requires (... and std::constructible_from<std::remove_cvref_t<decltype(t)>, decltype(ts)>)
+constexpr auto any_of = any<StaticArray<std::remove_cvref_t<decltype(t)>, 1 + sizeof...(ts)>{ t, ts... }>;
+
 template <StaticArray arr>
 constexpr auto none = NoneOf<detail::sort_and_unique<arr>()>{};
 
+template <auto t, auto... ts>
+requires (... and std::constructible_from<std::remove_cvref_t<decltype(t)>, decltype(ts)>)
+constexpr auto none_of = none<StaticArray<std::remove_cvref_t<decltype(t)>, 1 + sizeof...(ts)>{ t, ts... }>;
+
 template <StaticArray arr>
 constexpr auto all = AllOf<arr>{};
+
+template <auto t, auto... ts>
+requires (... and std::constructible_from<std::remove_cvref_t<decltype(t)>, decltype(ts)>)
+constexpr auto all_of = all<StaticArray<std::remove_cvref_t<decltype(t)>, 1 + sizeof...(ts)>{ t, ts... }>;
 
 template <StaticArray arr>
 constexpr auto ign = Ignore<AllOf<arr>>{};
@@ -38,13 +50,31 @@ consteval auto operator"" _any()
 }
 
 template <StaticArray arr>
+consteval auto operator"" _any_of()
+{
+	return any<arr>;
+}
+
+template <StaticArray arr>
 consteval auto operator"" _none()
 {
 	return none<arr>;
 }
 
 template <StaticArray arr>
+consteval auto operator"" _none_of()
+{
+	return none<arr>;
+}
+
+template <StaticArray arr>
 consteval auto operator"" _all()
+{
+	return all<arr>;
+}
+
+template <StaticArray arr>
+consteval auto operator"" _all_of()
 {
 	return all<arr>;
 }
@@ -60,13 +90,19 @@ consteval auto operator"" _ign()
 namespace k3::tok3n::operators {
 
 using ::k3::tok3n::any;
+using ::k3::tok3n::any_of;
 using ::k3::tok3n::none;
+using ::k3::tok3n::none_of;
 using ::k3::tok3n::all;
+using ::k3::tok3n::all_of;
 using ::k3::tok3n::ign;
 
 using ::k3::tok3n::operator""_any;
+using ::k3::tok3n::operator""_any_of;
 using ::k3::tok3n::operator""_none;
+using ::k3::tok3n::operator""_none_of;
 using ::k3::tok3n::operator""_all;
+using ::k3::tok3n::operator""_all_of;
 using ::k3::tok3n::operator""_ign;
 
 } // namespace k3::tok3n::operators
