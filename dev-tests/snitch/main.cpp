aaea92866a8a3267ae3c981b44e4a65d016fa000
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-03-10 17:55:06 -0500

    Run-time snitch

--- dev-tests/snitch/main.cpp
+++ dev-tests/snitch/main.cpp
@@ -1,7 +1,36 @@
 #include <snitch/snitch.hpp>
+#include <k3/tok3n.h>
 #include <iostream>
+#include <format>
 
-int main()
+using namespace k3::tok3n::operators;
+
+auto digit = "0123456789"_any_of;
+auto year = exactly<4>(digit) % join;
+auto month = exactly<2>(digit) % join;
+auto day = digit % exactly<2> % join;
+
+auto parser = year >> "-"_ign >> month >> "-"_ign >> day;
+
+#if 0
+TEST_CASE("ISO8601")
 {
-    std::cout << "test";
+    std::string_view input = "2024-01-01";
+    auto result = parser.parse(input);
+
+    REQUIRE(result.has_value());
+    CHECK(*result == std::tuple("2024", "01", "01"));
 }
+#else
+TEST_CASE("ISO8601")
+{
+    std::string_view input = "bad";
+    auto result = parser.parse(input);
+
+    INFO(std::format("Input could not be parsed.\nInput = \"{}\"\n"
+        "Parser = {}", input, k3::tok3n::pretty(parser).view()));
+
+    REQUIRE(result.has_value());
+    CHECK(*result == std::tuple("2024", "01", "01"));
+}
+#endif
