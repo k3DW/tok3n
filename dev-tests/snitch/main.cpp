2a49a3b586884d8327c7f39b1630d1ac0dcf2fa7
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-03-10 17:39:38 -0500

    Add snitch and UT as submodules

---
+++ dev-tests/snitch/main.cpp
@@ -0,0 +1,7 @@
+#include <snitch/snitch.hpp>
+#include <iostream>
+
+int main()
+{
+    std::cout << "test";
+}
---
+++ dev-tests/ut/main.cpp
@@ -0,0 +1,14 @@
+#define BOOST_UT_DISABLE_MODULE
+#include <boost/ut.hpp> // import boost.ut;
+
+constexpr auto sum(auto... values) { return (values + ...); }
+
+int main() {
+  using namespace boost::ut;
+
+  "sum"_test = [] {
+    expect(sum(0) == 0_i);
+    expect(sum(1, 2) == 3_i);
+    expect(sum(1, 2) > 0_i and 41_i == sum(40, 2));
+  };
+}
