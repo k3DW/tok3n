125bdbc4b0488757e1aa2de848553a4521414722
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-03-10 16:54:36 -0500

    Doctest

--- dev-tests/doctest/main.cpp
+++ dev-tests/doctest/main.cpp
@@ -1,15 +1,39 @@
 #include <doctest/doctest.h>
 #include <k3/tok3n.h>
 #include <iostream>
+#include <format>
 
-auto p = k3::tok3n::any<"123"> >> k3::tok3n::any<"abc">;
+using namespace k3::tok3n::operators;
 
-TEST_CASE("ABC")
+auto digit = "0123456789"_any_of;
+auto year = exactly<4>(digit) % join;
+auto month = exactly<2>(digit) % join;
+auto day = digit % exactly<2> % join;
+
+auto parser = year >> "-"_ign >> month >> "-"_ign >> day;
+
+#if 0
+TEST_CASE("ISO8601")
 {
-    CHECK(p.family == k3::tok3n::ParserFamily::Sequence);
-    CHECK(p.parse("1a").has_value());
-    CHECK(*p.parse("1a") == std::tuple("1", "a"));
+    std::string_view input = "2024-01-01";
+    auto result = parser.parse(input);
+
+    REQUIRE(result.has_value());
+    CHECK(*result == std::tuple("2024", "01", "01"));
 }
+#else
+TEST_CASE("ISO8601")
+{
+    std::string_view input = "bad";
+    auto result = parser.parse(input);
+
+    INFO(std::format("Input could not be parsed.\nInput = \"{}\"\n"
+        "Parser = {}", input, k3::tok3n::pretty(parser).view()));
+
+    REQUIRE(result.has_value());
+    CHECK(*result == std::tuple("2024", "01", "01"));
+}
+#endif
 
 // https://github.com/doctest/doctest/blob/master/doc/markdown/commandline.md
 int main(int argc, const char** argv)
