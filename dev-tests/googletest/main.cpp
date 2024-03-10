a75633affac1d7be57da80860a37aca84d1277f6
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-03-09 22:52:16 -0600

    GoogleTest

--- dev-tests/googletest/main.cpp
+++ dev-tests/googletest/main.cpp
@@ -1,14 +1,38 @@
 #include <gtest/gtest.h>
 #include <k3/tok3n.h>
-#include <iostream>
+#include <format>
 
-auto p = k3::tok3n::any<"123"> >> k3::tok3n::any<"abc">;
+using namespace k3::tok3n::operators;
 
-TEST(ABC, ABCDEFG)
+auto digit = "0123456789"_any_of;
+auto year = exactly<4>(digit) % join;
+auto month = exactly<2>(digit) % join;
+auto day = digit % exactly<2> % join;
+
+auto parser = year >> "-"_ign >> month >> "-"_ign >> day;
+
+//TEST(ISO8601, parse_result)
+//{
+//	auto result = parser.parse("2024-01-01");
+//
+//	ASSERT_TRUE(result.has_value());
+//	EXPECT_EQ(*result, std::tuple("2024", "01", "01"));
+//}
+
+TEST(ISO8601, parse_result)
 {
-    EXPECT_EQ(p.family, k3::tok3n::ParserFamily::Sequence);
-    EXPECT_TRUE(p.parse("1a").has_value());
-    EXPECT_EQ(*p.parse("1a"), std::tuple("1", "a"));
+	std::string_view input = "2024-01-01";
+	auto result = parser.parse(input);
+
+	ASSERT_TRUE(result.has_value())
+		<< std::format("Input could not be parsed.\nInput = \"{}\"\n"
+		"Parser = {}", input, k3::tok3n::pretty(parser).view());
+	EXPECT_EQ(*result, std::tuple("2024", "01", "01"));
+}
+
+TEST(ISO8601, combinator_result)
+{
+	//static_assert(std::same_as<decltype(month), decltype(year)>);
 }
 
 int main(int argc, char **argv)
