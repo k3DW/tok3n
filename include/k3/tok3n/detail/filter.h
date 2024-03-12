8050b8b580a82a316475a19db1d40d5cce7b9609
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-03-11 21:05:31 -0500

    #148: Reimplement detail::filter with traditional TMP, for some reason my previous method didn't work anymore in MSVC

--- include/k3/tok3n/detail/filter.h
+++ include/k3/tok3n/detail/filter.h
@@ -5,29 +5,28 @@
 
 namespace k3::tok3n::detail {
 
-inline constexpr auto filter_impl =
-	[]<class Self, template <class...> class List, type_predicate Pred, class... Done, class Head, class... Tail>
-		(Self self, Pred, std::type_identity<List<Done...>>, std::type_identity<Head>, std::type_identity<Tail>... tail) consteval
-	{
-		throw;
+template <type_predicate Pred, class ListOfDone, class Head, class... Tail>
+struct filter_impl {};
 
-		if constexpr (sizeof...(Tail) == 0)
-		{
-			if constexpr (Pred::template predicate<Head>::value)
-				return std::type_identity<List<Done..., Head>>{};
-			else
-				return std::type_identity<List<Done...>>{};
-		}
-		else
-		{
-			if constexpr (Pred::template predicate<Head>::value)
-				return self(self, Pred{}, std::type_identity<List<Done..., Head>>{}, tail...);
-			else
-				return self(self, Pred{}, std::type_identity<List<Done...>>{}, tail...);
-		}
-	};
+template <type_predicate Pred, template <class...> class List, class... Done, class Head>
+struct filter_impl<Pred, List<Done...>, Head>
+{
+	using type = std::conditional_t<Pred::template predicate<Head>::value,
+		List<Done..., Head>,
+		List<Done...>
+	>;
+};
+
+template <type_predicate Pred, template <class...> class List, class... Done, class Head, class... Tail>
+struct filter_impl<Pred, List<Done...>, Head, Tail...>
+{
+	using type = std::conditional_t<Pred::template predicate<Head>::value,
+		typename filter_impl<Pred, List<Done..., Head>, Tail...>::type,
+		typename filter_impl<Pred, List<Done...>, Tail...>::type
+	>;
+};
 
 template <type_predicate Pred, template <class...> class List, class... Ts>
-using filter = typename invoke_type<filter_impl, decltype(filter_impl), Pred, std::type_identity<List<>>, std::type_identity<Ts>...>::type;
+using filter = typename filter_impl<Pred, List<>, Ts...>::type;
 
 } // namespace k3::tok3n::detail
