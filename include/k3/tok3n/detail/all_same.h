51bf9b307b1f0bff635dc86d6ea75dbc4dd3ab21
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-06-07 13:10:38 -0500

    #166: Fix usage of packs

--- include/k3/tok3n/detail/all_same.h
+++ include/k3/tok3n/detail/all_same.h
@@ -4,6 +4,12 @@
 namespace k3::tok3n::detail {
 
 template <class T, class... Ts>
-concept all_same = (... && std::same_as<T, Ts>);
+consteval auto all_same_impl()
+{
+    return (... && std::same_as<T, Ts>);
+}
+
+template <class... Ts>
+concept all_same = all_same_impl<Ts...>();
 
 } // namespace k3::tok3n::detail
