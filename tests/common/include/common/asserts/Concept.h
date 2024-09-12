617fab85644271801c5d0a902f72d4c3a57bbce8
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-09-11 22:28:33 -0500

    Fully qualify detail namespace in asserts headers

--- tests/common/include/common/asserts/Concept.h
+++ tests/common/include/common/asserts/Concept.h
@@ -11,19 +11,19 @@
 		"`" STR(__VA_ARGS__) "` satisfies the " STR(Concept) " concept, but should not.")
 
 #define ASSERT_IS_PARSER(P, V, PARSER_TYPE, ...)                            \
-	ASSERT_CONCEPT(detail::parser, P);                                      \
+	ASSERT_CONCEPT(k3::tok3n::detail::parser, P);                           \
 	ASSERT(P::family == PARSER_TYPE,                                        \
 		"`" STR(P) "::family` does not equal " STR(PARSER_TYPE));           \
-	ASSERT((detail::parser_for<P, V>),                                      \
+	ASSERT((k3::tok3n::detail::parser_for<P, V>),                           \
 		"`parser_for<" STR(P) ", " STR(V) ">` is not satisfied");           \
 	ASSERT((std::same_as<typename P::template result_for<V>, __VA_ARGS__>), \
 		"`" STR(P) "::result_for<" STR(V) ">` is not " STR(__VA_ARGS__))
 
 #define ASSERT_IS_NOT_PARSER(P, V, PARSER_TYPE)                                   \
-	ASSERT_CONCEPT(detail::parser, P);                                            \
+	ASSERT_CONCEPT(k3::tok3n::detail::parser, P);                                 \
 	ASSERT(P::family == PARSER_TYPE,                                              \
 		"`" STR(P) "::family` does not equal " STR(PARSER_TYPE));                 \
-	ASSERT(not (detail::parser_for<P, V>),                                        \
+	ASSERT(not (k3::tok3n::detail::parser_for<P, V>),                             \
 		"`parser_for<" STR(P) ", " STR(V) ">` is satisfied but it should not be");
 
 #define ASSERT_PARSER_VALUE_TYPE(P, V)                         \
--- tests/common/include/common/asserts/Equality.h
+++ tests/common/include/common/asserts/Equality.h
@@ -11,16 +11,16 @@ concept same_values = std::same_as<std::remove_cvref_t<decltype(lhs)>, std::remo
 // This wouldn't be needed if we had CWG2518 or P2593
 
 #define DEP_ASSERT_PARSER_VALUES_EQ(LHS_VALUE, RHS_VALUE, LHS_DISPLAY, RHS_DISPLAY)          \
-	ASSERT((detail::parser<decltype(LHS_VALUE)>),                                            \
-		"`" STR(LHS_DISPLAY) "` does not satisfy the detail::parser concept.");              \
-	ASSERT((detail::parser<decltype(RHS_VALUE)>),                                            \
-		"`" STR(RHS_DISPLAY) "` does not satisfy the detail::parser concept.");              \
+	ASSERT((k3::tok3n::detail::parser<decltype(LHS_VALUE)>),                                 \
+		"`" STR(LHS_DISPLAY) "` does not satisfy the parser concept.");                      \
+	ASSERT((k3::tok3n::detail::parser<decltype(RHS_VALUE)>),                                 \
+		"`" STR(RHS_DISPLAY) "` does not satisfy the parser concept.");                      \
 	ASSERT((same_values<(LHS_VALUE), (RHS_VALUE)>),                                          \
 		"`" STR(LHS_DISPLAY) "` and `" STR(RHS_DISPLAY) "` are not the same, but should be")
 
 #define DEP_ASSERT_PARSER_VALUES_NE(LHS_VALUE, RHS_VALUE, LHS_DISPLAY, RHS_DISPLAY)          \
-	ASSERT_CONCEPT(detail::parser, decltype(LHS_VALUE));                                     \
-	ASSERT_CONCEPT(detail::parser, decltype(RHS_VALUE));                                     \
+	ASSERT_CONCEPT(k3::tok3n::detail::parser, decltype(LHS_VALUE));                          \
+	ASSERT_CONCEPT(k3::tok3n::detail::parser, decltype(RHS_VALUE));                          \
 	ASSERT((not same_values<(LHS_VALUE), (RHS_VALUE)>),                                      \
 		"`" STR(LHS_DISPLAY) "` and `" STR(RHS_DISPLAY) "` are the same, but should not be")
 
@@ -35,13 +35,13 @@ concept same_values = std::same_as<std::remove_cvref_t<decltype(lhs)>, std::remo
 	DEP_ASSERT_PARSER_VALUES_NE(LHS_VALUE, RHS_VALUE, LHS_VALUE, RHS_VALUE)
 
 #define ASSERT_MODIFIER_VALUES_EQ(LHS_VALUE, RHS_VALUE)                                  \
-	ASSERT_CONCEPT(detail::modifier, decltype(LHS_VALUE));                               \
-	ASSERT_CONCEPT(detail::modifier, decltype(RHS_VALUE));                               \
+	ASSERT_CONCEPT(k3::tok3n::detail::modifier, decltype(LHS_VALUE));                    \
+	ASSERT_CONCEPT(k3::tok3n::detail::modifier, decltype(RHS_VALUE));                    \
 	ASSERT((same_values<(LHS_VALUE), (RHS_VALUE)>),                                      \
 		"`" STR(LHS_VALUE) "` and `" STR(RHS_VALUE) "` are not the same, but should be")
 
 #define ASSERT_MODIFIER_VALUES_NE(LHS_VALUE, RHS_VALUE)                                  \
-	ASSERT_CONCEPT(detail::modifier, decltype(LHS_VALUE));                               \
-	ASSERT_CONCEPT(detail::modifier, decltype(RHS_VALUE));                               \
+	ASSERT_CONCEPT(k3::tok3n::detail::modifier, decltype(LHS_VALUE));                    \
+	ASSERT_CONCEPT(k3::tok3n::detail::modifier, decltype(RHS_VALUE));                    \
 	ASSERT((not same_values<(LHS_VALUE), (RHS_VALUE)>),                                  \
 		"`" STR(LHS_VALUE) "` and `" STR(RHS_VALUE) "` are the same, but should not be")
