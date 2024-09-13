c4003321279b6104a2ccf453cd4df8e4e533bd45
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-09-12 21:49:33 -0500

    Put maybe, one_or_more, and zero_or_more operators into detail namespace

--- include/k3/tok3n/operators.h
+++ include/k3/tok3n/operators.h
@@ -2,17 +2,20 @@
 
 #include <k3/tok3n/detail/operators/choice.h>
 #include <k3/tok3n/detail/operators/epsilon.h>
-#include <k3/tok3n/operators/maybe.h>
+#include <k3/tok3n/detail/operators/maybe.h>
 #include <k3/tok3n/detail/operators/not.h>
 #include <k3/tok3n/detail/operators/sequence.h>
-#include <k3/tok3n/operators/one_or_more.h>
-#include <k3/tok3n/operators/zero_or_more.h>
+#include <k3/tok3n/detail/operators/one_or_more.h>
+#include <k3/tok3n/detail/operators/zero_or_more.h>
 
 namespace k3::tok3n {
 
 // This is temporary
 using k3::tok3n::detail::operator|;
+using k3::tok3n::detail::operator~;
 using k3::tok3n::detail::operator>>;
+using k3::tok3n::detail::operator+;
+using k3::tok3n::detail::operator*;
 
 } // namespace k3::tok3n
 
