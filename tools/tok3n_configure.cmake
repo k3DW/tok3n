b28903567d3edeac3ef54b91c7ac487e075b2daf
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-09-23 23:51:21 -0500

    #196: Fix pch sorting with function tok3n_configure_ide

--- tools/tok3n_configure.cmake
+++ tools/tok3n_configure.cmake
@@ -21,27 +21,23 @@ macro(tok3n_configure_target target visibility)
 
 endmacro()
 
-function(tok3n_configure_ide list_of_files)
+function(tok3n_configure_ide target)
 
-    if (TOK3N_SETUP_IDE)
+    get_target_property(list_of_files ${target} SOURCES)
 
-        foreach(_source IN ITEMS ${list_of_files})
-            get_filename_component(_source_path "${_source}" PATH)
-            file(RELATIVE_PATH _source_path_rel "${CMAKE_CURRENT_LIST_DIR}" "${_source_path}")
-            string(REPLACE "/" "\\" _group_path "${_source_path_rel}")
-            source_group("${_group_path}" FILES "${_source}")
+    foreach(_source IN ITEMS ${list_of_files})
+        get_filename_component(_source_path "${_source}" DIRECTORY)
+        file(RELATIVE_PATH _source_path_rel "${CMAKE_CURRENT_LIST_DIR}" "${_source_path}")
+        string(REPLACE "/" "\\" _group_path "${_source_path_rel}")
+        source_group("${_group_path}" FILES "${_source}")
+    endforeach()
+
+    if (EXISTS "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target}.dir/cmake_pch.cxx")
+        # https://stackoverflow.com/questions/71070320/cmake-how-to-group-generated-precompiled-headers-in-visual-studio
+        source_group("_cmake_generated" FILES "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target}.dir/cmake_pch.cxx")
+        foreach(config_type ${CMAKE_CONFIGURATION_TYPES})
+            source_group("_cmake_generated" FILES "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${target}.dir/${config_type}/cmake_pch.hxx")
         endforeach()
-
-        if (EXISTS "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/tests.dir/cmake_pch.cxx")
-
-            # https://stackoverflow.com/questions/71070320/cmake-how-to-group-generated-precompiled-headers-in-visual-studio
-            source_group("_cmake_generated" FILES ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/tests.dir/cmake_pch.cxx)
-            foreach(config_type ${CMAKE_CONFIGURATION_TYPES})
-                source_group("_cmake_generated" FILES ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/tests.dir/${config_type}/cmake_pch.hxx)
-            endforeach()
-
-        endif()
-
     endif()
 
 endfunction()
