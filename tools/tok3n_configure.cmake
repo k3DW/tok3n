90ec3a9ed5def2e425da678755e1547423cf474d
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-09-29 12:13:55 -0500

    #196: Add suggested MSVC warnings from cppbestpractices

--- tools/tok3n_configure.cmake
+++ tools/tok3n_configure.cmake
@@ -16,6 +16,26 @@ macro(tok3n_configure_target target visibility)
         $<$<CXX_COMPILER_ID:MSVC>:
             /permissive- /volatile:iso /Zc:preprocessor
             /WX /W4
+            /w14640
+            /w14242
+            /w14254
+            /w14263
+            /w14265
+            /w14287
+            /we4289
+            /w14296
+            /w14311
+            /w14545
+            /w14546
+            /w14547
+            /w14549
+            /w14555
+            /w14619
+            /w14640
+            /w14826
+            /w14905
+            /w14906
+            /w14928
         >
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:GNU>>:
             -Werror
