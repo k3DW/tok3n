672d1e11e905ad3c71ba8869c1a2559ae5f18870
Braden Ganetsky <braden.ganetsky@gmail.com>
2024-10-23 22:43:07 -0500

    Work around GCC ICE

--- include/k3/tok3n/detail/modifiers/sub.h
+++ include/k3/tok3n/detail/modifiers/sub.h
@@ -14,43 +14,39 @@ namespace k3::tok3n::detail {
 namespace impl {
 
 template <class T, substitution Sub>
-requires (not parser<T>)
-constexpr auto SubstituteOne(T, Sub)
-	-> T;
-
-template <template <static_array> class Basic, static_array arr, substitution Sub>
-requires parser<Basic<arr>>
-constexpr auto SubstituteOne(Basic<arr>, Sub)
-	-> Basic<arr>;
+struct substitute_one
+{
+	using type = T;
+};
 
 template <parser P, static_array str, substitution Sub>
-constexpr parser auto SubstituteOne(named_parser<P, str>, Sub);
+requires (Sub::name == str)
+struct substitute_one<named_parser<P, str>, Sub>
+{
+	using type = decltype(Sub::mod(typename substitute_one<P, Sub>::type{}));
+};
+
+template <parser P, static_array str, substitution Sub>
+requires (Sub::name != str)
+struct substitute_one<named_parser<P, str>, Sub>
+{
+	using type = named_parser<typename substitute_one<P, Sub>::type, str>;
+};
 
 template <template <class...> class Template, class... Args, substitution Sub>
-requires parser<Template<Args...>>
-constexpr parser auto SubstituteOne(Template<Args...>, Sub)
+struct substitute_one<Template<Args...>, Sub>
 {
-	return Template<decltype(SubstituteOne(Args{}, Sub{}))...>{};
-}
-
-template <parser P, static_array str, substitution Sub>
-constexpr parser auto SubstituteOne(named_parser<P, str>, Sub)
-{
-	using InnerSubbed = decltype(SubstituteOne(P{}, Sub{}));
-	if constexpr (Sub::name == str)
-		return Sub::mod(InnerSubbed{});
-	else
-		return named_parser<InnerSubbed, str>{};
-}
+	using type = Template<typename substitute_one<Args, Sub>::type...>;
+};
 
 template <parser P, substitution Sub, substitution... Subs>
-constexpr parser auto Substitute(P, Sub, Subs...)
+constexpr parser auto substitute(P, Sub, Subs...)
 {
-	using Subbed = decltype(SubstituteOne(P{}, Sub{}));
+	using Subbed = typename substitute_one<P, Sub>::type;
 	if constexpr (sizeof...(Subs) == 0)
 		return Subbed{};
 	else
-		return decltype(Substitute(Subbed{}, Subs{}...)){};
+		return substitute(Subbed{}, Subs{}...);
 }
 
 } // namespace impl
@@ -62,7 +58,7 @@ struct sub_modifier final : modifier_base
 	template <parser P, substitution Sub, substitution... Subs>
 	constexpr auto operator()(P, Sub, Subs...) const
 	{
-		return decltype(impl::Substitute(P{}, Sub{}, Subs{}...)){};
+		return impl::substitute(P{}, Sub{}, Subs{}...);
 	}
 
 	template <substitution Sub, substitution... Subs>
@@ -73,7 +69,7 @@ struct sub_modifier final : modifier_base
 		template <parser P>
 		constexpr auto operator()(P) const
 		{
-			return decltype(impl::Substitute(P{}, Sub{}, Subs{}...)){};
+			return impl::substitute(P{}, Sub{}, Subs{}...);
 		}
 	};
 
