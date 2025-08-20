aca86c36672d6886fe3c87117a284d6f4fd3c1b1
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-08-19 23:15:37 -0500

    Workaround for Clang 16

--- tests/framework/include/framework/Assert.hpp
+++ tests/framework/include/framework/Assert.hpp
@@ -15,12 +15,14 @@
 
 
 
-#define ASSERT_COMPILE_TIME(CONDITION, MESSAGE)                                         \
-	STATIC_ASSERT(CONDITION, #CONDITION);                                               \
-	if (                                                                                \
-		bool _ct = TestResultContext::check(std::bool_constant<(CONDITION)>::value);    \
-		not _ct                                                                         \
-	)                                                                                   \
+// The template argument `[&]{ return bool(CONDITION); }()` is a workaround for Clang 16
+
+#define ASSERT_COMPILE_TIME(CONDITION, MESSAGE)                                                           \
+	STATIC_ASSERT(CONDITION, #CONDITION);                                                                 \
+	if (                                                                                                  \
+		bool _ct = TestResultContext::check(std::bool_constant<[&]{ return bool(CONDITION); }()>::value); \
+		not _ct                                                                                           \
+	)                                                                                                     \
 		return TestResultContext::add_error(_ct, true, MESSAGE, Error::Fatality::fatal)
 
 #define ASSERT_RUN_TIME(CONDITION, MESSAGE)                                             \
@@ -30,21 +32,21 @@
 	)                                                                                   \
 		return TestResultContext::add_error(true, _rt, MESSAGE, Error::Fatality::fatal)
 
-#define ASSERT_COMPILE_AND_RUN_TIME(CONDITION, MESSAGE)                                \
-	STATIC_ASSERT(CONDITION, #CONDITION);                                              \
-	if (                                                                               \
-		bool _ct = TestResultContext::check(std::bool_constant<(CONDITION)>::value),   \
-		     _rt = TestResultContext::check(CONDITION);                                \
-		not _ct or not _rt                                                             \
-	)                                                                                  \
+#define ASSERT_COMPILE_AND_RUN_TIME(CONDITION, MESSAGE)                                                   \
+	STATIC_ASSERT(CONDITION, #CONDITION);                                                                 \
+	if (                                                                                                  \
+		bool _ct = TestResultContext::check(std::bool_constant<[&]{ return bool(CONDITION); }()>::value), \
+		     _rt = TestResultContext::check(CONDITION);                                                   \
+		not _ct or not _rt                                                                                \
+	)                                                                                                     \
 		return TestResultContext::add_error(_ct, _rt, MESSAGE, Error::Fatality::fatal)
 
-#define EXPECT_COMPILE_TIME(CONDITION, MESSAGE)                                      \
-	STATIC_ASSERT(CONDITION, #CONDITION);                                            \
-	if (                                                                             \
-		bool _ct = TestResultContext::check(std::bool_constant<(CONDITION)>::value); \
-		not _ct                                                                      \
-	)                                                                                \
+#define EXPECT_COMPILE_TIME(CONDITION, MESSAGE)                                                           \
+	STATIC_ASSERT(CONDITION, #CONDITION);                                                                 \
+	if (                                                                                                  \
+		bool _ct = TestResultContext::check(std::bool_constant<[&]{ return bool(CONDITION); }()>::value); \
+		not _ct                                                                                           \
+	)                                                                                                     \
 		TestResultContext::add_error(_ct, true, MESSAGE, Error::Fatality::non_fatal)
 
 #define EXPECT_RUN_TIME(CONDITION, MESSAGE)                                          \
@@ -54,13 +56,13 @@
 	)                                                                                \
 		TestResultContext::add_error(true, _rt, MESSAGE, Error::Fatality::non_fatal)
 
-#define EXPECT_COMPILE_AND_RUN_TIME(CONDITION, MESSAGE)                              \
-	STATIC_ASSERT(CONDITION, #CONDITION);                                            \
-	if (                                                                             \
-		bool _ct = TestResultContext::check(std::bool_constant<(CONDITION)>::value), \
-		     _rt = TestResultContext::check(CONDITION);                              \
-		not _ct or not _rt                                                           \
-	)                                                                                \
+#define EXPECT_COMPILE_AND_RUN_TIME(CONDITION, MESSAGE)                                                   \
+	STATIC_ASSERT(CONDITION, #CONDITION);                                                                 \
+	if (                                                                                                  \
+		bool _ct = TestResultContext::check(std::bool_constant<[&]{ return bool(CONDITION); }()>::value), \
+		     _rt = TestResultContext::check(CONDITION);                                                   \
+		not _ct or not _rt                                                                                \
+	)                                                                                                     \
 		TestResultContext::add_error(_ct, _rt, MESSAGE, Error::Fatality::fatal)
 
 
