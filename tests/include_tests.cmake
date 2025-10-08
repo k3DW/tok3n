6ff7bfd923becfc0c094af72133e7f48222dc51b
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-10-07 20:08:53 -0500

    Simplify test discovery with file locking

--- tests/include_tests.cmake
+++ tests/include_tests.cmake
@@ -1,4 +1,4 @@
-# Copyright 2024 Braden Ganetsky
+# Copyright 2024-2025 Braden Ganetsky
 # Distributed under the Boost Software License, Version 1.0.
 # https://www.boost.org/LICENSE_1_0.txt
 
@@ -8,8 +8,12 @@
 
 set(CTEST_FILE "${TEST_DIRECTORY}/CTestTestfile.cmake")
 
+file(LOCK "${CTEST_FILE}.lock" GUARD PROCESS TIMEOUT 15)
+
 file(READ "${CTEST_FILE}" ctest_file_contents)
 string(FIND "${ctest_file_contents}" "${LIST_OF_TESTS_FILE}" found_position)
 if(found_position EQUAL -1)
     file(APPEND "${CTEST_FILE}" "include(${LIST_OF_TESTS_FILE})\n")
 endif()
+
+file(LOCK "${CTEST_FILE}.lock" RELEASE)
