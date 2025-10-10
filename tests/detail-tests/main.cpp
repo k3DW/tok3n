82db3233ea9a573de13c3ecdd465c224647091e0
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-10-09 19:37:08 -0500

    Create detail_tests target

---
+++ tests/detail-tests/main.cpp
@@ -0,0 +1,11 @@
+// Copyright 2025 Braden Ganetsky
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include "framework/Runner.hpp"
+
+int main(int argc, const char* argv[])
+{
+	auto& runner = Runner::get();
+	return runner.exec(argc, argv);
+}
