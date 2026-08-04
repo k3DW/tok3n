766b97e0930a1c704b476fd30507fd43657bc6a8
Braden Ganetsky <braden.ganetsky@gmail.com>
2026-08-03 23:10:45 -0500

    Update copyright year

--- include/k3/tok3n/span.hpp
+++ include/k3/tok3n/span.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023-2025 Braden Ganetsky
+// Copyright 2023-2026 Braden Ganetsky
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
